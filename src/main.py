d7a525c5c078f58ab7bc0f47c7577cc3f45327e8
Javen <javen.william@gmail.com>
2025-06-17 15:16:41 -0400

    impl train_and_visualize() and train_model flag

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ import matplotlib.patches as patches
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(777)
+train_model = False
 
 # Hyperparameters
 batch_size = 8
@@ -43,7 +44,7 @@ val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_w
 test_dataset = WheatDataset(mode='test')
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=n_workers, collate_fn=collate_fn)
 
-# Load model
+# Initialize model
 model_weights = "models/fasterrcnn_resnet50_fpn_COCO-V1.pt"
 model = fasterrcnn_resnet50_fpn(
     weights=FasterRCNN_ResNet50_FPN_Weights.COCO_V1,
@@ -51,61 +52,60 @@ model = fasterrcnn_resnet50_fpn(
 )
 in_features = model.roi_heads.box_predictor.cls_score.in_features
 model.roi_heads.box_predictor = FastRCNNPredictor(in_channels=in_features, num_classes=2) # 1 class (wheat) + background
-
-# Load custom weights if they exist
-if os.path.exists(model_weights):
-    model.load_state_dict(torch.load(model_weights, weights_only=True))
 model.to(device)
 
 # Optimizer
 optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0.0005)
 
 # Training loop
-for epoch in range(n_epochs):
-    # Train
-    model.train()
-    train_loss = 0.0
-    for batch in tqdm(train_loader, leave=False, desc=f"Train epoch {epoch+1}/{n_epochs}"):
-        # Extract batch items
-        images, targets = batch
-        images = [image.to(device) for image in images]
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        # Forward-feed and back-propagate
-        loss_dict = model(images, targets) # Returns losses
-        losses = sum(loss for loss in loss_dict.values())
-        optimizer.zero_grad()
-        losses.backward()
-        optimizer.step()
-        train_loss += losses.item()
-    print(f"Epoch {epoch+1}/{n_epochs}, Train Loss: {train_loss / len(train_loader):.4f}")
-
-    # Validate
-    model.eval()
-    metric = MeanAveragePrecision(iou_thresholds=[0.5 + i * 0.05 for i in range(6)])  # 0.5 to 0.75
-    with torch.no_grad():
-        for batch in tqdm(val_loader, leave=False, desc=f"Val epoch {epoch + 1}/{n_epochs}"):
+def train_and_validate():
+    for epoch in range(n_epochs):
+        # Train
+        model.train()
+        train_loss = 0.0
+        for batch in tqdm(train_loader, leave=False, desc=f"Train epoch {epoch+1}/{n_epochs}"):
             # Extract batch items
             images, targets = batch
             images = [image.to(device) for image in images]
             targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-            # Evaluate
-            outputs = model(images)
-            preds = [
-                {
-                    'boxes': output['boxes'],
-                    'scores': output['scores'],
-                    'labels': output['labels'],
-                } for output in outputs
-            ]
-            metric.update(preds, targets)
-    mAP = metric.compute()['map'].item()
-    print(f"Epoch {epoch + 1}/{n_epochs}, Val: {mAP:.4f}")
+            # Forward-feed and back-propagate
+            loss_dict = model(images, targets) # Returns losses
+            losses = sum(loss for loss in loss_dict.values())
+            optimizer.zero_grad()
+            losses.backward()
+            optimizer.step()
+            train_loss += losses.item()
+        print(f"\nEpoch {epoch+1}/{n_epochs}, Train Loss: {train_loss / len(train_loader):.4f}")
 
-# Save model
-torch.save(model.state_dict(), model_weights)
+        # Validate
+        model.eval()
+        metric = MeanAveragePrecision(iou_thresholds=[0.5 + i * 0.05 for i in range(6)])  # 0.5 to 0.75
+        with torch.no_grad():
+            for batch in tqdm(val_loader, leave=False, desc=f"Val epoch {epoch + 1}/{n_epochs}"):
+                # Extract batch items
+                images, targets = batch
+                images = [image.to(device) for image in images]
+                targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+                # Evaluate
+                outputs = model(images)
+                preds = [
+                    {
+                        'boxes': output['boxes'],
+                        'scores': output['scores'],
+                        'labels': output['labels'],
+                    } for output in outputs
+                ]
+                metric.update(preds, targets)
+        mAP = metric.compute()['map'].item()
+        print(f"Epoch {epoch + 1}/{n_epochs}, Val: {mAP:.4f}")
+
+# Train & Save model
+if train_model:
+    train_and_validate()
+    torch.save(model.state_dict(), model_weights)
 
 # Inference step for test dataset
-def test_predict(model, test_loader, output_file="submission.csv"):
+def predict_test(output_file="output/submission.csv"):
     model.eval()
     predictions = []
     with torch.no_grad():
@@ -143,7 +143,12 @@ def visualize_predictions(image, boxes, scores, threshold=0.5):
             ax.text(x_min, y_min, f'{score:.2f}', color='white', fontsize=8, backgroundcolor='red')
     plt.show()
 
-test_predict(model, test_loader)  # output_file="/kaggle/working/submission.csv"
+# Load fine-tuned weights if they exist
+if os.path.exists(model_weights):
+    model.load_state_dict(torch.load(model_weights, weights_only=True))
+
+# Predict test dataset
+predict_test(output_file="output/submission.csv")  # output_file="/kaggle/working/submission.csv"
 
 
 
